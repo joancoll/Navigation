f36bd097eb6ff548388c1c952cce07ef0ac17d60
joancoll <joancoll@inspladelestany.cat>
2024-12-04 18:52:40 +0100

    1st commit

---
+++ app/src/main/java/cat/dam/andy/navigation/MainActivity.kt
@@ -0,0 +1,127 @@
+package cat.dam.andy.navigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.*
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp()
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    val navController = rememberNavController()
+    val selectedButton = remember { mutableIntStateOf(1) }
+    Column(modifier = Modifier.systemBarsPadding()
+        .fillMaxSize()) {
+        TopMenu(navController = navController, selectedButton = selectedButton)
+        NavigationHost(navController = navController)
+    }
+}
+
+@Composable
+fun TopMenu(navController: NavHostController, selectedButton: MutableState<Int>) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Gray)
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        (1..4).forEach { index ->
+            Button(
+                onClick = {
+                    navController.navigate("layout$index") {
+                        popUpTo(navController.graph.startDestinationId) { saveState = true }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                    selectedButton.value = index
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = if (selectedButton.value == index) Color.Blue else Color.LightGray
+                ),
+                shape = RoundedCornerShape(50)
+            ) {
+                Text("Layout$index")
+            }
+        }
+    }
+}
+
+@Composable
+fun NavigationHost(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "layout1") {
+        composable("layout1") { Layout1() }
+        composable("layout2") { Layout2() }
+        composable("layout3") { Layout3() }
+        composable("layout4") { Layout4() }
+    }
+}
+
+@Composable
+fun Layout1() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Red),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Layout 1", color = Color.White)
+    }
+}
+
+@Composable
+fun Layout2() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Green),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Layout 2", color = Color.White)
+    }
+}
+
+@Composable
+fun Layout3() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Yellow),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Layout 3", color = Color.Black)
+    }
+}
+
+@Composable
+fun Layout4() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Cyan),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Layout 4", color = Color.Black)
+    }
+}

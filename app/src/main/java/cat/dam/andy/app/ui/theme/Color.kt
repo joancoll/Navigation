6b67f1e75448a74885001a86602d46c14d054cba
joancoll <joancoll@inspladelestany.cat>
2024-12-18 10:28:24 +0100

    Color updated

--- app/src/main/java/cat/dam/andy/app/ui/theme/Color.kt
+++ app/src/main/java/cat/dam/andy/app/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package cat.dam.andy.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -9,3 +10,28 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// afegir colors
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val Teal700 = Color(0xFF018786)
+
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val Silver = Color(0xFFC0C0C0)
+val Gray = Color(0xFF808080)
+val Red = Color(0xFFFF0000)
+val Maroon = Color(0xFF800000)
+val Orange = Color(0xFFFFA500)
+val Yellow = Color(0xFFFFFF00)
+val Olive = Color(0xFF808000)
+val Lime = Color(0xFF00FF00)
+val Green = Color(0xFF008000)
+val Aqua = Color(0xFF00FFFF)
+val Teal = Color(0xFF008080)
+val Blue = Color(0xFF0000FF)
+val Navy = Color(0xFF000080)
+val Fuchsia = Color(0xFFFF00FF)
+val Purple = Color(0xFF800080)

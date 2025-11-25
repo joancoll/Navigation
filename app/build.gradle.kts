d06426be570aebc05a681287b7c24727b4109f96
joancoll <joancoll@inspladelestany.cat>
2025-11-25 15:56:19 +0100

    targetSdk 16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "cat.dam.andy.app"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "cat.dam.andy.app"
         minSdk = 28
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
